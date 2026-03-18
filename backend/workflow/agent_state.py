3ba1188d6ae4882339b6ba789fd570b49f45ceeb
jay-isampelliwar <isampelliwar79@gmail.com>
2026-03-18 15:50:59 +0530

    Add required fields documentation in AgentState class, clarifying budget, source, destination, travel duration, and travel date for trip planning.

--- backend/workflow/agent_state.py
+++ backend/workflow/agent_state.py
@@ -2,14 +2,26 @@ from typing import Optional
 from langgraph.graph import MessagesState
 
 class AgentState(MessagesState):
+
+    """
+    budget: The budget for the trip
+    source: The source of the trip
+    destination: The destination of the trip
+    travel_duration: The duration of the trip
+    travel_date: The date of the trip
+
+    These 5 required fields to be set before the trip can be planned.
+    """
+
+    budget: Optional[str]
+    source: Optional[str]
+    destination: Optional[str]
+    travel_duration: Optional[int]
+    travel_date: Optional[str]
     current_date_time: Optional[str]
     travel_timings: Optional[dict]
     transportation: Optional[dict]
     things_to_do: Optional[dict]
     travel_tips: Optional[dict]
-    source: Optional[str]
-    destination: Optional[str]
-    travel_duration: Optional[int]
-    travel_date: Optional[str]
     full_trip_plan: Optional[str]
     web_search_queries: Optional[list[str]]
